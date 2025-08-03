9a07785e7ddcb07bc7dd770d676d8b0b15bada1f
ugurhs <ugurcandgn46@icloud.com>
2025-08-03 16:07:55 +0300

    history delete

--- .history/resources/views/dashboard.blade_20250803151102.php
+++
@@ -1,7 +0,0 @@
-<x-layouts.app :title="__('Dashboard')">
-    <div class="container mx-auto px-4 py-8">
-        <h1 class="text-2xl font-bold mb-6">{{ __('Dashboard') }}</h1>
-
-
-    </div>
-</x-layouts.app>
